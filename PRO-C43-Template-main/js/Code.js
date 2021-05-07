15df2a642a8b11d9918ce0c4cce0d21f259a0667
MonomitaBhattacharjee <ujjwalabhattacharjee@gmail.com>
2021-05-07 21:41:59 +0530

    button added

---
+++ PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode3 = "FUNCTION";
+const accessCode2 = "DATABASE";
+
+function clues() {
+
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100, 50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100, 70)
+    
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 700, 140);
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 700, 160)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 100, 280);
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,300)
+
+
+}
